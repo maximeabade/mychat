412dd09e53050636e1a5f5104063b5ec833bd3d5
maximeabade <maximeabade@gmail.com>
2022-09-11 19:05:29 +0200

    b

--- client/src/index.js
+++ client/src/index.js
@@ -3,11 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <App />
-);
+const rootELement = document.getElementById('root');
+const root = ReactDOM.createRoot(rootELement);
+root.render(<App />);
 
 
 
