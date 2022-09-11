0be087662b43229547d46232fb997075adeac1d0
maximeabade <maximeabade@gmail.com>
2022-09-11 19:37:22 +0200

    bvvbbv

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,18 @@
-import './App.css';
-import io from 'socket.io-client';
-import { useState} from 'react';
+import "./App.css";
+import io from "socket.io-client";
+import { useState } from "react";
 import Chat from "./Chat";
 
-const socket = io('http://localhost:3000');
-console.log(socket);
-
+const socket = io.connect("http://localhost:3001");
 
 function App() {
-
-  //state
-  const [username,setUsername] = useState("");
-  const [room,setRoom] = useState("");
-  const [showChat,setShowChat] = useState(false);
-
-  //action
+  const [username, setUsername] = useState("");
+  const [room, setRoom] = useState("");
+  const [showChat, setShowChat] = useState(false);
 
   const joinRoom = () => {
-    if(username !== "" && room !== ""){
-      socket.emit("join_room",room);
+    if (username !== "" && room !== "") {
+      socket.emit("join_room", room);
       setShowChat(true);
     }
   };
@@ -26,21 +20,26 @@ function App() {
   return (
     <div className="App">
       {!showChat ? (
-      <div className="joinChatContainer">
-        <h3>Join the chat</h3>
-        <input type="text" placeholder="Enter your name" 
-          onChange={(event) => {
-            setUsername(event.target.value)
-          }} 
-        />
-        <input type="text" placeholder="Room ID" 
-          onChange={(event) => {
-            setRoom(event.target.value)
-          }} 
-        />
-        <button onClick={joinRoom}>Join</button>
-      </div> ) : (    
-        <Chat socket={socket} username={username}  room={room}/>
+        <div className="joinChatContainer">
+          <h3>Join A Chat</h3>
+          <input
+            type="text"
+            placeholder="John..."
+            onChange={(event) => {
+              setUsername(event.target.value);
+            }}
+          />
+          <input
+            type="text"
+            placeholder="Room ID..."
+            onChange={(event) => {
+              setRoom(event.target.value);
+            }}
+          />
+          <button onClick={joinRoom}>Join A Room</button>
+        </div>
+      ) : (
+        <Chat socket={socket} username={username} room={room} />
       )}
     </div>
   );
--- server/dist/index.dev.js
+++ server/dist/index.dev.js
@@ -1,12 +1,12 @@
+"use strict";
 
-
-var express = require('express');
+var express = require("express");
 
 var app = express();
 
-var http = require('http');
+var http = require("http");
 
-var cors = require('cors');
+var cors = require("cors");
 
 var _require = require("socket.io"),
     Server = _require.Server;
@@ -16,20 +16,22 @@ var server = http.createServer(app);
 var io = new Server(server, {
   cors: {
     origin: "http://localhost:3000",
-    methods: ["GET", "DELETE"]
+    methods: ["GET", "POST"]
   }
 });
 io.on("connection", function (socket) {
+  console.log("User Connected: ".concat(socket.id));
   socket.on("join_room", function (data) {
     socket.join(data);
+    console.log("User with ID: ".concat(socket.id, " joined room: ").concat(data));
   });
   socket.on("send_message", function (data) {
     socket.to(data.room).emit("receive_message", data);
   });
-  socket.on(disconnect, function () {
-    console.log("disconnect", socket.id);
+  socket.on("disconnect", function () {
+    console.log("User Disconnected", socket.id);
   });
 });
 server.listen(3000, function () {
-  console.log("Server is running on port 3000");
+  console.log("SERVER RUNNING");
 });
